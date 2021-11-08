ce5154c77270fd13496d86198ef7328460852bc9
mkwawama <79860095+mkwawama@users.noreply.github.com>
2021-11-08 16:40:35 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,37 @@
+letMySong, amplitude;
+
+function preload(){
+    mySong = loadSound('assets/massiveAttackAngel.mp3');
+}
 function setup() {
-    var canvas = createCanvas(800, 800);
+    var canvas = createCanvas(window.innerWidth, window.innerHeight);
     canvas.parent("p5container");
+    mySong.play();
+    amplitude=new p5.Amplitude();
 }
 
 function draw() {
-    background(220);
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    let level = amplitude.getLevel();
+    let col = map(level, 0,1,0,255);
+    let vol= map(level,0,1,0,1000);
+    console.log(col);
+    background(col);
+    
+    noStroke();
+
+    if(vol >=200){
+        fill(col +10);
+        ellipse(width/2, height/2, vol+180);
+    }
+
+    fill(col,255,255);
+    ellipse(width/2,height/2,vol);
+
+    fill(255,col,255);
+    ellipse(width/2,height/2,vol/2);
+
+    
+    
+    
 }
